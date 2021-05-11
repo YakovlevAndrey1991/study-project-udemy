80bafdcd33a7318358400977c4c2df364a8003bb
Andrey Yakovlev <ultima.pank@gmail.com>
2021-05-11 19:19:31 +0300

    change XML to Fetch

--- js/script.js
+++ js/script.js
@@ -242,12 +242,10 @@ window.addEventListener('DOMContentLoaded', () => {
                 display: block;
                 margin: 0 auto;
             `;
-            // form.append(statusMessage);
-            form.insertAdjacentElement('afterend',statusMessage);
+            form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            request.setRequestHeader('Content-type', 'application/json');
+            // const request = new XMLHttpRequest();
+            // request.open('POST', 'server.php');
 
             const formData = new FormData(form);
 
@@ -256,19 +254,27 @@ window.addEventListener('DOMContentLoaded', () => {
                 object[key] = value;
             });
 
-            const json = JSON.stringify(object);
 
-            request.send(json);
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
+            fetch('server.php', {
+                method: "POST",
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            })
+                .then(data => data.text())
+                .then(data => {
+                    console.log(data);
                     showThanksModal(message.success);
-                    form.reset();
                     statusMessage.remove();
-                } else {
-                    showThanksModal(message.failure);
-                }
-            });
+                }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
+            })
+
+
+
         });
     }
 
@@ -293,7 +299,6 @@ window.addEventListener('DOMContentLoaded', () => {
             prevModalDialog.classList.remove('hide');
             closeModal();
         }, 4000)
-
     }
 
 
